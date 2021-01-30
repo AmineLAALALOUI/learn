d6f79dfad53e9a28ee501c6a860f1354d80006f1
AmineLAALAOUI <amine.laalaoui@outlook.com>
2021-01-30 17:44:37 +0100

    v7

--- Main.java
+++ Main.java
@@ -3,10 +3,7 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Main {
-<<<<<<< HEAD
-    public static void main(String[] args) {
-        System.out.println("This will be printed");
-=======
+
   public static void main(String[] args) {
 
     System.out.println("Enter something here : ");
@@ -16,7 +13,6 @@ public class Main {
         String s = bufferRead.readLine();
 
         System.out.println(s);
->>>>>>> branch1
     }
     catch(IOException e)
     {
